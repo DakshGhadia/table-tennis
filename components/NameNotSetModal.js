d6e99ff54f47470534106d502db3937217fa609e
Daksh  Ghadia <dakshghadia@Dakshs-MacBook-Air.local>
2024-08-12 13:12:25 -0400

    Fixed a bug

--- components/NameNotSetModal.js
+++ components/NameNotSetModal.js
@@ -5,7 +5,7 @@ export default function NameNotSetModal({onPress}) {
         <div className="fixed inset-0 flex items-center justify-center z-50">
           <div className="bg-white p-6 rounded-lg shadow-lg">
             <p className="mb-4 text-center text-lg font-semibold">
-              Please enter both the players' names first
+              {`Please enter both the players' names first`}
             </p>
             <div className="flex justify-center gap-x-4">
               <button
